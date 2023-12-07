60c59c51f6cfc718937820710b7f59a0ded17303
lex-nagorny <alex.nagorny.next@gmail.com>
2023-12-07 12:39:46 +0300

    debbug

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,10 +1,10 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
-function createColorString(array) {
-   const flag= array.filter(el=>{
+function createColorString() {
+   const flag= colors.filter(el=>{
       return el === "черный" || el === "красный" || el === "желтый"
    })
    return flag.join('-')
 }
 
-console.log(createColorString(colors));
+console.log(createColorString());
